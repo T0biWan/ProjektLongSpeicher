a44f77ecfee31879d0db096a1834b312dd5c68ca
Tobi Wan <Tobi.Klatt@web.de>
2015-11-01 13:20:22 +0100

    Alle Methoden implementiert scheinen zu funktionieren Kleine ABweichungen zu Ingas Code

--- src/Tests.java
+++ src/Tests.java
@@ -5,11 +5,32 @@ public class Tests extends Werkstatt {
       // TODO Auto-generated constructor stub
    }
 
-   public static void main(String [] args) {
+
+
+   public static void main(String[] args) {
       // Attribute
-      Werkstatt a = new Werkstatt(5);
+      Werkstatt w = new Werkstatt(5);
+      int zuVielFürsArray = 6;
 
       // Tests
+      // int a = 1;
+      // int b = 2;
+      // int c = 3;
+      // int d = 4;
+      // int e = 5;
+
+      // int a = 5;
+      // int b = 4;
+      // int c = 3;
+      // int d = 2;
+      // int e = 1;
+
+      int a = 5;
+      int b = 3;
+      int c = 1;
+      int d = 5;
+      int e = 7;
+
       // Nicht im Speicher
       // 0 liefert n = 0 zurück
       // 9 liefert 6, nächster freier speicher zurück...?
@@ -28,22 +49,79 @@ public class Tests extends Werkstatt {
       // pln(a.index(4));
       // pln(a.index(5));
 
-      pln(a.toString());
-      a.fuegeEin(1);
-      pln(a.toString());
+      pln("Array vor dem Einfügen: " + w.toString());
+      w.fuegeEin(a);
+      pln("Array nach dem Einfügen: " + w.toString());
+      pln("lbi: " + w.lbi);
+      pln();
 
-      a.fuegeEin(2);
-      pln(a.toString());
+      pln("Array vor dem Einfügen: " + w.toString());
+      w.fuegeEin(b);
+      pln("Array nach dem Einfügen: " + w.toString());
+      pln("lbi: " + w.lbi);
+      pln();
 
-      a.fuegeEin(3);
-      pln(a.toString());
+      pln("Array vor dem Einfügen: " + w.toString());
+      w.fuegeEin(c);
+      pln("Array nach dem Einfügen: " + w.toString());
+      pln("lbi: " + w.lbi);
+      pln();
 
-      a.fuegeEin(4);
-      pln(a.toString());
+      pln("Array vor dem Einfügen: " + w.toString());
+      w.fuegeEin(d);
+      pln("Array nach dem Einfügen: " + w.toString());
+      pln("lbi: " + w.lbi);
+      pln();
 
-      a.fuegeEin(5);
-      pln(a.toString());
+      pln("Array vor dem Einfügen: " + w.toString());
+      w.fuegeEin(e);
+      pln("Array nach dem Einfügen: " + w.toString());
+      pln("lbi: " + w.lbi);
+      pln();
 
+      pln("Array vor dem Einfügen: " + w.toString());
+      w.fuegeEin(zuVielFürsArray);
+      pln("Array nach dem Einfügen: " + w.toString());
+      pln("lbi: " + w.lbi);
+      pln();
+
+      // a.fuegeEin(2);
+      // pln(a.toString());
+      // System.out.println("lbi: " + a.lbi);
+      //
+      // a.fuegeEin(5);
+      // pln(a.toString());
+      // System.out.println("lbi: " + a.lbi);
+      //
+      // a.fuegeEin(4);
+      // pln(a.toString());
+      // System.out.println("lbi: " + a.lbi);
+      //
+      // a.fuegeEin(5);
+      // pln(a.toString());
+      // System.out.println("lbi: " + a.lbi);
+      //
+      // a.fuegeEin(5);
+      // pln(a.toString());
+      // System.out.println("lbi: " + a.lbi);
+
+      pln("Array vor dem Löschen: " + w.toString());
+      w.loesche(5);
+      pln("Array nach dem Löschen: " + w.toString());
+      pln("lbi: " + w.lbi);
+      pln();
+
+      pln("Array vor dem Löschen: " + w.toString());
+      w.loesche(5);
+      pln("Array nach dem Löschen: " + w.toString());
+      pln("lbi: " + w.lbi);
+      pln();
+
+      pln("Array vor dem Einfügen: " + w.toString());
+      w.fuegeEin(e);
+      pln("Array nach dem Einfügen: " + w.toString());
+      pln("lbi: " + w.lbi);
+      pln();
    }
 
 }
--- src/Werkstatt.java
+++ src/Werkstatt.java
@@ -1,13 +1,15 @@
 public class Werkstatt {
    // Konstruktor
    public Werkstatt(int arrayLaenge) {
-      speicher = new long [arrayLaenge];
+      speicher = new long[arrayLaenge];
    }
 
    // Attribute
-   // private long [] speicher = { 2, 3, 4, 5, 6 };
-   private long [] speicher;
-   private int     lbi = -1;
+   // private long [] speicher = { 2, 3, 5, 6};
+   private long[] speicher;
+   public int     lbi = -1;
+
+
 
    // Methoden
    public String toString() {
@@ -18,7 +20,8 @@ public class Werkstatt {
       // "[10]" // 1
       // "[20, 30, 10]" // 3
 
-      if (lbi == -1) return "[]";
+      if (lbi == -1)
+         return "[]";
       StringBuilder sb = new StringBuilder();
       sb.append("[" + speicher[0]);
       for (int i = 1; i <= lbi; i++) {
@@ -29,9 +32,12 @@ public class Werkstatt {
       return sb.toString();
    }
 
+
+
    public int index(long n) {
       int von = 0;
-      int bis = lbi; // lbi ist nicht der tatsächlcih belegte index.
+      int bis = lbi;
+      // lbi ist nicht der tatsächlcih belegte index.
       // lbi ist jetzt die füllhöhe des arrays
       while (von <= bis) {
          int mitte = von + (bis - von) / 2;
@@ -46,52 +52,130 @@ public class Werkstatt {
       return von; // n steht nicht im speicher
    }
 
+
+
    public boolean fuegeEin(long n) {
       // idee:
       // das array vom letzten belegten index an bis zum ersten durchgehen
       // wenn die enthaltene Zahl größer ist als die einzufuegende Zahl
       // dann wird sie um einen index nach rechts, in plus richtung, verschoben
       // ist die nächst zahl kleiner als die einzufuegende zahl, kann eingefuegt
-      // werden
-      // am frei gewordenen index
-      if (lbi < speicher.length) {
+      // werden am frei gewordenen index
+      if (lbi + 1 < speicher.length) {
          if (lbi == -1) {
             speicher[lbi + 1] = n;
          } else {
             for (int i = lbi; i > -1; i--) {
-               if (lt(n, i)) {
+               if (lt(n, speicher[i])) {
                   speicher[i + 1] = speicher[i];
-               } else {
+                  speicher[i] = n;
+               } else if (ge(n, speicher[i])) {
                   speicher[i + 1] = n;
                   lbi++;
                   return true;
                }
             }
          }
-
          lbi++;
+         return true;
+
+      }
+      return false;
+   }
+
+
+
+   public boolean istDrin(long n) {
+      // Liefert true wenn n in diesem Speicher vorkommt, und sonst false.
+      int index = index(n);
+      if (index <= lbi && speicher[index] == n)
+         return true;
+      return false;
+   }
+
+
+
+   public boolean loesche(long n) {
+      //      Achtung: Inga benutzt nicht istDrin um zu prüfen!
+      if (istDrin(n)) {
+         int index = index(n);
+         for (int i = index; i <= lbi; i++) {
+            if (i + 1 <= lbi) {
+               speicher[i] = speicher[i + 1];
+            }
+         }
+
+         lbi--;
          return true;
       }
       return false;
    }
 
+
+
    // Geerbte Methoden aus AbstractLongSpeicher
    // Methoden mit kurzen Namen:
-   // @formatter:off   Hier ist nur eine Zeile pro Methode vorteilhaft
-   static void printf(String s, Object... ob) {System.out.printf (s, ob);}
-   static void pln(Object ob)                 {System.out.println(   ob);}
-   static void p  (Object ob)                 {System.out.print  (   ob);}
-   static void pln()                          {System.out.println(     );}
+   // Hier ist nur eine Zeile pro Methode vorteilhaft
+   static void printf(String s, Object... ob) {
+      System.out.printf(s, ob);
+   }
+
+
+
+   static void pln(Object ob) {
+      System.out.println(ob);
+   }
+
+
+
+   static void p(Object ob) {
+      System.out.print(ob);
+   }
+
+
+
+   static void pln() {
+      System.out.println();
+   }
+
+
 
    // long-Werte, die in einen long-Speicher eingefuegt werden, sollen
    // nicht mit den Operatoren <, <=, == etc, verglichen werden, sondern
    // mit den folgenden Funktionen lt, le, eq etc.
    // Dadurch wird es leichter, den Typ long (spaeter einmal) durch einen
    // anderen Typ (z.B. int oder String oder ...) zu ersetzen.
-   static boolean lt(long a, long b) {return a <  b;} // less then
-   static boolean le(long a, long b) {return a <= b;} // less or equal
-   static boolean eq(long a, long b) {return a == b;} // equal
-   static boolean ge(long a, long b) {return a >= b;} // greater or equal
-   static boolean gt(long a, long b) {return a >  b;} // greater
-   static boolean ne(long a, long b) {return a != b;} // not equal
+   static boolean lt(long a, long b) {
+      return a < b;
+   } // less then
+
+
+
+   static boolean le(long a, long b) {
+      return a <= b;
+   } // less or equal
+
+
+
+   static boolean eq(long a, long b) {
+      return a == b;
+   } // equal
+
+
+
+   static boolean ge(long a, long b) {
+      return a >= b;
+   } // greater or equal
+
+
+
+   static boolean gt(long a, long b) {
+      return a > b;
+   } // greater
+
+
+
+   static boolean ne(long a, long b) {
+      return a != b;
+   } // not equal
 }
