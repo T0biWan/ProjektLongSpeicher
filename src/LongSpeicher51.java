665ec61880da2c5ccee53a27caff2b16c683b8e7
Andreas Austing <aausting1@gmail.com>
2015-12-27 20:55:47 +0100

    Implementierung LongSpeicher51 fertiggestellt

--- src/LongSpeicher51.java
+++ src/LongSpeicher51.java
@@ -41,33 +41,33 @@ class LongSpeicher51 extends AbstractLongSpeicher {
          this.rub = new Knoten[] { rub };
       }
    } // class Knoten
-     // ---------------------------------------------------------------------
-     // Ein leerer Baum besteht aus einem End-Dummy-Knoten EDK und einer
-     // Knoten-Reihung AR (der Laenge 1). AR[0] ist anfangs gleich dem EDK
-     // und ist spaeter der erste "richtige Knoten" (die Wurzel) des Baums
+   // ---------------------------------------------------------------------
+   // Ein leerer Baum besteht aus einem End-Dummy-Knoten EDK und einer
+   // Knoten-Reihung AR (der Laenge 1). AR[0] ist anfangs gleich dem EDK
+   // und ist spaeter der erste "richtige Knoten" (die Wurzel) des Baums
 
    private final Knoten   EDK = new Knoten(0, null, null);
    private final Knoten[] AR  = new Knoten[] { EDK };
 
    // ---------------------------------------------------------------------
-   private Knoten[] vorgaenger(long n) {
+   Knoten[] vorgaenger(long n) {
       // Liefert eine Knoten-Reihung r (der Laenge 1), die Teil eines
       // Knotens dieser Sammlung ist, und fuer die gilt:
       // r[0] ist der n-Knoten (falls es einen solchen gibt) und sonst
       // der EDK.
       // Die gesuchte Zahl n in den End-Dummy-Knoten eintragen
       // (spaetestens dort wird sie dann gefunden)
-      EDK.data = n;
+      //EDK.data = n;
       return vorgaengerR(n, AR);
    }
 
    private Knoten[] vorgaengerR(long n, Knoten[] hier) {
       // Eine rekursive Methode. Erledigt, was vorgaenger (ohne R)
       // versprochen hat (sollte nur von vorgaenger aufgerufen werden).
+      if (hier[0] == EDK) return hier;
       if(lt(hier[0].data, n)) return vorgaengerR(n, hier[0].lub);
       if(gt(hier[0].data, n)) return vorgaengerR(n, hier[0].rub);
       return hier;
- 
    }
 
    // ---------------------------------------------------------------------
@@ -80,12 +80,23 @@ class LongSpeicher51 extends AbstractLongSpeicher {
       //   --               "[]"
       //   10               "[10]"
       //   10, 20, 30       "[10, 20, 30]"
-      return "Noch nicht implementiert!"; // MUSS ERSETZT WERDEN
+      StringBuilder builder = new StringBuilder();
+      builder.append('[');
+      toStringR(AR, builder);
+      builder.append(']');
+      return builder.toString();
    }
 
    private void toStringR(Knoten[] hier, StringBuilder sb) {
       // Rekurisve Hilfsmethode fuer toString.
-      return; // MUSS ERSETZT WERDEN
+      Knoten knoten = hier[0];
+      if (knoten == EDK) return;
+
+      sb.append(knoten.data);
+      if (knoten.lub[0] != EDK) sb.append(", ");
+      toStringR(knoten.lub, sb);
+      if (knoten.rub[0] != EDK) sb.append(", ");
+      toStringR(knoten.rub, sb);
    }
 
    // ---------------------------------------------------------------------
@@ -93,11 +104,20 @@ class LongSpeicher51 extends AbstractLongSpeicher {
    public boolean fuegeEin(long n) {
       // Liefert false, wenn n bereits in dieser Sammlung vorkommt.
       // Fuegt sonst n in diesen Speicher ein und liefert true.
-      
-      Knoten vorgaenger = vorgaenger(n)[0];
-      if(vorgaenger != EDK) return false;
-      vorgaenger = new Knoten(n, EDK, EDK);
+
+      Knoten[] vorgaengerReihung = vorgaenger(n);
+      if(vorgaengerReihung[0] != EDK) return false;
+
+      Knoten neuerKnoten = new Knoten(n, EDK, EDK);
+      vorgaengerReihung[0] = neuerKnoten;
       return true;
+   }
+
+   private void fuegeEin(Knoten knoten) {
+      if (knoten == EDK) return;
+      fuegeEin(knoten.data);
+      fuegeEin(knoten.lub[0]);
+      fuegeEin(knoten.rub[0]);
    }
 
    // ---------------------------------------------------------------------
@@ -105,37 +125,35 @@ class LongSpeicher51 extends AbstractLongSpeicher {
    public boolean loesche(long n) {
       // Liefert false, wenn n in diesem Speicher nicht vorkommt.
       // Loescht sonst den Knoten, der n enthaelt und liefert true.
-      
+
+      Knoten[] knotenReferenz = vorgaenger(n);
+      Knoten knoten = knotenReferenz[0];
+
       // Wenn n gar nicht im Baum vorkommt, false zurückgeben
-      Knoten vorgaenger = vorgaenger(n)[0];
-      if(vorgaenger == EDK) return false;
-      
-      // Einfacher Fall
-      if(vorgaenger.lub[0] == EDK && vorgaenger.rub[0] == EDK) {
-         vorgaenger = EDK;
+      if(knoten == EDK) return false;
+
+      // Einfacher Fall - unser Knoten ist ein Blatt. Referenz auf EDK setzen.
+      if(knoten.lub[0] == EDK && knoten.rub[0] == EDK) {
+         knotenReferenz[0] = EDK;
          return true;
       }
-      
-      if(vorgaenger.lub[0] == EDK) {
-         vorgaenger = vorgaenger.rub[0];
+
+      // Fall Teilbaum (1) - unser Knoten hat nur einen Teilbaum rechts
+      if(knoten.lub[0] == EDK) {
+         knotenReferenz[0] = knoten.rub[0];
          return true;
       }
-      
-      if(vorgaenger.rub[0] == EDK) {
-         vorgaenger = vorgaenger.lub[0];
+
+      // Fall Teilbaum (2) - unser Knoten hat nur einen Teilbaum links
+      if(knoten.rub[0] == EDK) {
+         knotenReferenz[0] = knoten.lub[0];
          return true;
       }
-      
-      // Komplizierter Fall
-      Knoten aktuellerKnoten = vorgaenger;
-      vorgaenger = vorgaenger.lub[0];
-      while(vorgaenger.rub[0] != EDK) {
-         vorgaenger = vorgaenger.rub[0];
-      }
-      
-      aktuellerKnoten = vorgaenger;
-      vorgaenger = EDK;
-      return true; 
+
+      // Komplizierter Fall: Linker Teilbaum wird umgehängt; rechter Teilbaum neu hinzugefügt.
+      knotenReferenz[0] = knoten.lub[0];
+      fuegeEin(knoten.rub[0]);
+      return true;
    }
 
    // ---------------------------------------------------------------------
@@ -177,12 +195,12 @@ class LongSpeicher51 extends AbstractLongSpeicher {
       // Ein Ende des Baumes erreicht?
       if (hier[0] == EDK) return;
       // Gib den linken Unterbaum aus:
-      printR(hier[0].rub, einrueck + EINRUECK);
+      printR(hier[0].lub, einrueck + EINRUECK);
       // Gib den aktuellen Knoten aus:
       printf("%s", einrueck);
       printf("%2d%n", hier[0].data);
       // Gib den rechten Unterbaum aus:
-      printR(hier[0].lub, einrueck + EINRUECK);
+      printR(hier[0].rub, einrueck + EINRUECK);
    }
 
    // ---------------------------------------------------------------------
@@ -221,5 +239,5 @@ class LongSpeicher51 extends AbstractLongSpeicher {
       printf("H ------------------------------ H%n");
       printf("LongSpeicher51: Das war's erstmal!%n%n");
    } // main
-     // ---------------------------------------------------------------------
+   // ---------------------------------------------------------------------
 } // class LongSpeicher51
